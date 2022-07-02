73c0c25886ef4c7a8d5d027630214155afb70215
Fuzail <89026669+FOZAEL@users.noreply.github.com>
2022-07-02 09:30:54 +0430

    Update settings.py

--- toy/toy/settings.py
+++ toy/toy/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-m1vd(7_904+-a^umw6o_y4dac8yhq)ya4oyb2bamc!@fg^puj4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = "True"
 
 ALLOWED_HOSTS = ['*']
 
